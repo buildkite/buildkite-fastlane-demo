2513797af72854d8cf4158f9533ae493dd47cf13
Tim Lucas <t@toolmantim.com>
2015-08-08 19:21:30 +1000

    Initial Commit

---
+++ Buildkite Fastlane Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buildkite Fastlane Demo
+//
+//  Created by Tim Lucas on 8/08/2015.
+//  Copyright © 2015 Buildkite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
