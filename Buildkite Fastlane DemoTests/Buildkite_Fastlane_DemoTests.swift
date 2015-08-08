10343ad324fc0e59741913d4518b92ec54240369
Tim Lucas <t@toolmantim.com>
2015-08-08 20:29:48 +1000

    Add some UI and some tests

--- Buildkite Fastlane DemoTests/Buildkite_Fastlane_DemoTests.swift
+++ Buildkite Fastlane DemoTests/Buildkite_Fastlane_DemoTests.swift
@@ -13,23 +13,19 @@ class Buildkite_Fastlane_DemoTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(1 == 1)
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measureBlock {
-            // Put the code you want to measure the time of here.
+            1 == 1
         }
     }
     
